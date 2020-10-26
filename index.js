dc6b71e0f2858b134109eff53d6e52e0fc148dc7
Property404 <martinez.dagan@gmail.com>
2020-10-25 19:37:31 -0500

    Fixed bug: user couldn't enable long descriptions
    Telnet data was sent at the start, and that was passed
    to the BF interpreter. The fix was to reject all binary data

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const tokens = Brainfuck.tokenize(lk);
 console.log("Finished tokenizing");
 
 const server = net.createServer(function(socket) {
-	console.log("New connection");
+	console.log(`New connection from ${socket.address().address}`);
+	
 	const bf = new Brainfuck();
 	bf.output_handler = socket.write.bind(socket);
+
 	bf.interpret(tokens);
 
 	socket.on("data", function(chunk){
 		const data = chunk.toString();
+		
+		// Reject binary data
+		const initial = data.charCodeAt(0);
+		if(initial != 10 && initial != 13 &&
+			(initial<32 || initial > 128))
+			return;
+
 		console.log("Recv: ", data)
 		bf.absorb(data);
 	});
